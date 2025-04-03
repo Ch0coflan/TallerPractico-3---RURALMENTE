b67e5cb2c0010ebcaffba8e403349413e30e7927
DanRGon <correodedanny@gmail.com>
2025-04-02 19:01:07 -0500

    Correccion UI Map

--- Assets/Scripts/UI Map/SceneManager.cs
+++ Assets/Scripts/UI Map/SceneManager.cs
@@ -10,29 +10,36 @@ public class SceneManager : MonoBehaviour
     public GameObject current;
  
     public void TransformToImage(GameObject target){
-        StartCoroutine(FadeInAndOut(target));
-    }
- 
-    IEnumerator FadeInAndOut(GameObject target){
-        int fadeSpeed = 3;
-        Image transitionImage = transitionGO.GetComponent<Image>();
-        transitionImage.raycastTarget = true;
-        Color transitionColor = transitionImage.color;
- 
-        while (isFadingIn) {
-            transitionColor.a += fadeSpeed * Time.deltaTime;
-            if (transitionColor.a >= 1) {
-                isFadingIn = false;
-                transitionColor.a = 1;
-            }
-            transitionImage.color = transitionColor;
-            yield return new WaitForEndOfFrame();
-        }
-        transitionImage.raycastTarget = false;
-        current.SetActive(false);
         target.SetActive(true);
-        current = target;
- 
+        current.SetActive(false);
+        
+        /* Image transitionImage = current.GetComponent<Image>();
+         // transitionImage.raycastTarget = true;
+         transitionImage.sprite = target.GetComponent<SpriteRenderer>().sprite;
+        StartCoroutine(FadeInAndOut(target));*/
+     }
+/*
+     IEnumerator FadeInAndOut(GameObject target){
+         int fadeSpeed = 3;
+
+         /*
+         Color transitionColor = transitionImage.color;
+
+         while (isFadingIn) {
+             transitionColor.a += fadeSpeed * Time.deltaTime;
+             if (transitionColor.a >= 1) {
+                 isFadingIn = false;
+                 transitionColor.a = 1;
+             }
+             transitionImage.color = transitionColor;
+             yield return new WaitForEndOfFrame();
+         }
+         transitionImage.raycastTarget = false;
+         */
+       /* current.SetActive(false);
+        target.SetActive(true);
+        current = target;*/
+ /*
         while (!isFadingIn) {
             transitionColor.a -= fadeSpeed * Time.deltaTime;
             if (transitionColor.a <= 0) {
@@ -42,6 +49,6 @@ public class SceneManager : MonoBehaviour
             transitionImage.color = transitionColor;
             yield return new WaitForEndOfFrame();
         }
-        
-    }
+        */
+    
 }
